3ec59de1a75691d294cb6190ff2d2669a7844d5e
deepakgit1 <dakshgaidhane1@gmal.com>
2022-03-22 18:39:14 +0530

    make it better

--- client/src/components/SingleChat.js
+++ client/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import Lottie from "lottie-react";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT  = "http://localhost:5000";
+const ENDPOINT  = "https://hello-chat-app-new.herokuapp.com/"
 
 let socket,selectedChatCompare;
 
