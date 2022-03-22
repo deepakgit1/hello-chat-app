f4c3ed2b8a12023442b9a98c2fdf3ff61d202f00
deepakgit1 <dakshgaidhane1@gmal.com>
2022-03-22 21:12:18 +0530

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes")
 const chatRoutes = require("./routes/chatRoutes")
 const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+const path = require("path");
 
 
 dotenv.config()
@@ -22,16 +23,31 @@ app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
 
-app.use(notFound)
-app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
 
-//for Deployement process
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "./client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running 123..");
+  });
 }
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const server = app.listen(PORT, console.log(`Server Started at port ${PORT}`))
 
